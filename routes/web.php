b23257a47f6632edd83d143fdb111036b8a3c49b
Alfiramhaamalia <alfiaml08@gmail.com>
2025-10-07 17:51:11 +0700

    Tugas Pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/profile/{nama}/{npm}/{kelas}',[ProfileController::class,'profile']);
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/create', [UserController::class, 'create']) -> name(name: 'user.create');
+Route::post('/user', [UserController::class, 'store'])->name(name : 'user.store');
+Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::resource('user', UserController::class);
